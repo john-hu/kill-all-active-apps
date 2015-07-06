c3274a52e4829946fb2d136e815f19df0074acd4
John Hu <im@john.hu>
2015-07-06 10:59:30 +0800

    zip folder into zip file

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@ var gulp = require('gulp');
 var zip = require('gulp-zip');
 
 gulp.task('default', function() {
-  return gulp.src(['main.js', 'manifest.webapp', 'LICENSE', 'icons/*'])
+  return gulp.src(['main.js', 'manifest.webapp', 'LICENSE', '**/icons/*'])
     .pipe(zip('application.zip'))
     .pipe(gulp.dest('./'));
 });
