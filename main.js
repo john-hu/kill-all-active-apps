2184da92b7e589aeba9cbc5938dcb45bee26c55f
John Hu <im@john.hu>
2015-07-08 22:44:33 +0800

    update to support latest system app

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
 (function() {
   function createKillAllButton() {
+    if (document.documentElement.dataset.killAllButtonInjected) {
+      console.log('kill-all-button is already injected');
+      return;
+    }
     var cardsView = document.getElementById('cards-view');
 
     var button = document.createElement('button');
@@ -13,24 +17,30 @@
     button.style.position = 'absolute';
     button.style.top = '3rem';
     button.style.right = '3rem';
-
-    button.addEventListener('touchstart', function() {
+    button.style.zIndex = '9999999';
+    button.addEventListener('touchstart', function(e) {
       var taskMgr = window.wrappedJSObject.appWindowManager.taskManager;
       if (taskMgr.cardsList.children.length > 0) {
-        for (var appId in taskMgr.cardsByAppID) {
-          taskMgr.cardsByAppID[appId].killApp();
-        }
+        var cards = Array.from(taskMgr.cardsList.children).map(function(e) {
+          return taskMgr.getCardForElement(e);
+        });
+        cards.forEach(function(card) {
+          if (card.app.killable()) {
+            card.killApp();
+          }
+        });
         taskMgr.exitToApp();
       }
     }, true);
 
-    window.addEventListener('taskmanager-activated', function() {
+    window.addEventListener('cardviewshown', function() {
       var taskMgr = window.wrappedJSObject.appWindowManager.taskManager;
       button.style.display = taskMgr.cardsList.children.length === 0 ?
                                 'none' : 'inline-block';
     });
 
     cardsView.insertBefore(button, cardsView.firstElementChild);
+    document.documentElement.dataset.killAllButtonInjected = true;
   }
 
   if (document.readyState !== 'loading') {
